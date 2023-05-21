3d4dc6124ec332ab8cd5a55f1299467cd8ffa8c9
Precioussoul <habsoff@gmail.com>
2023-05-21 18:26:05 +0100

    feat: -implemented categories filter navigations

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,11 +1,45 @@
 "use client"
 
+import {useRouter, useSearchParams} from "next/navigation"
 import {CategoryBoxProps} from "../types"
+import {useCallback} from "react"
+import qs from "query-string"
 
 const CategoryBox = ({icon: Icon, label, selected}: CategoryBoxProps) => {
+  const router = useRouter()
+  const params = useSearchParams()
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {}
+
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    }
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      {skipNull: true}
+    )
+
+    router.push(url)
+  }, [label, params, router])
+
   return (
     <div
-      className={`flex flex-col items-center justify-center gap-2 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
+      onClick={handleClick}
+      className={`flex flex-col items-center justify-center gap-2 border-b-2 p-3 hover:text-neutral-800 transition cursor-pointer ${
         selected ? "border-b-neutral-800" : "border-transparent"
       } ${selected ? "text-neutral-800" : "text-neutral-500"} `}
     >
