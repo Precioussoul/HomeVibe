6cf0f410f803b85a6ff60c0d69f784ddbb28c24a
Precioussoul <habsoff@gmail.com>
2023-05-28 13:19:17 +0100

    feat: added onAdd and onReduce function to increment and decrement our rents properties counter

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,9 +1,21 @@
 "use client"
 
+import {useCallback} from "react"
 import {CounterProps} from "../types"
 
-const Counter = ({}: CounterProps) => {
-  return <div>Counter</div>
+const Counter = ({title, subtitle, value, onChange}: CounterProps) => {
+  const onAdd = useCallback(() => {
+    onChange(value + 1)
+  }, [onChange, value])
+
+  const onReduce = useCallback(() => {
+    if (value === 1) {
+      return
+    }
+    onChange(value + 1)
+  }, [onChange, value])
+
+  return <div></div>
 }
 
 export default Counter
