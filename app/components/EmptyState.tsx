87f9cf77193c19a0408e9a75192119dcb379104c
Precioussoul <habsoff@gmail.com>
2023-05-29 22:33:48 +0100

    update: updated and completed EmptyState

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -3,6 +3,7 @@
 import {useRouter} from "next/navigation"
 import {EmptyState} from "../types"
 import Heading from "./Heading"
+import Button from "./Button"
 
 const EmptyState = ({
   title = "No exact matches",
@@ -14,6 +15,15 @@ const EmptyState = ({
   return (
     <div className='h-[60vh] flex flex-col gap-2 justify-center items-center'>
       <Heading center title={title} subtitle={subtitle} />
+      <div className='w-40 mt-4'>
+        {showReset && (
+          <Button
+            outline
+            label='Remove all filters'
+            onClick={() => router.push("/")}
+          />
+        )}
+      </div>
     </div>
   )
 }
