8f06e08cc01c9588a8133290e0edd4d1bb14320e
Precioussoul <habsoff@gmail.com>
2023-06-03 20:04:06 +0100

    feat: creatings list of functions to be executed in ListingCard component

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import useCountries from "@/app/hooks/useCountries"
 import {ListingCardProps} from "@/app/types"
+import {useRouter} from "next/navigation"
+import {useCallback, useMemo} from "react"
 
 const ListingCard = ({
   data,
@@ -8,9 +11,33 @@ const ListingCard = ({
   onAction,
   disabled,
   actionLabel,
-  actionId,
+  actionId = "",
   currentUser,
 }: ListingCardProps) => {
+  const router = useRouter()
+  const {getByValue} = useCountries()
+  const location = getByValue(data.locationValue)
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation()
+
+      if (disabled) {
+        return
+      }
+
+      onAction?.(actionId)
+    },
+    [onAction, actionId, disabled]
+  )
+
+  const price = useMemo(() => {
+    if (reservations) {
+      return reservations.totalPrice
+    }
+    return data.price
+  }, [reservations, data.price])
+
   return <div>ListingCard</div>
 }
 
