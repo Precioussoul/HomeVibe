76325e26c265708a4f547028be86a6c00c702a18
Precioussoul <habsoff@gmail.com>
2023-06-03 23:25:31 +0100

    feat: completed HeartButton component for ListingCard

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,9 +1,24 @@
 "use client"
 import React from "react"
 import {HeartButtonProps} from "../types"
+import {AiOutlineHeart} from "react-icons/ai"
 
 const HeartButton = ({listingId, currentUser}: HeartButtonProps) => {
-  return <div>HeartButton</div>
+  const hasFavorited = false
+  const toggleFavorite = () => {}
+  return (
+    <div
+      onClick={toggleFavorite}
+      className='relative hover:opacity-80 transition cursor-pointer'
+    >
+      <AiOutlineHeart
+        size={28}
+        className={`fill-white absolute -top-[2px] -right-[2px] ${
+          hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"
+        }`}
+      />
+    </div>
+  )
 }
 
 export default HeartButton
