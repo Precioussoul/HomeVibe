9eeb63aea7f433a80d6c215b04d68785c45d92c8
Precioussoul <habsoff@gmail.com>
2023-06-06 23:54:32 +0100

    feat: updated ListingPage component

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,8 +1,15 @@
-"use client"
-import React from "react"
+import getListingId from "@/app/actions/getListingByIds"
+import EmptyState from "@/app/components/EmptyState"
+import {IParams} from "@/app/types"
 
-const ListingPage = () => {
-  return <div>ListingPage</div>
+const ListingPage = async ({params}: {params: IParams}) => {
+  const listing = await getListingId(params)
+
+  if (!listing) {
+    return <EmptyState />
+  }
+
+  return <div>{listing?.title}</div>
 }
 
 export default ListingPage
