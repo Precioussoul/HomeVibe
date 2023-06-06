464c4abeae67e736147bee981e228410870b00e1
Precioussoul <habsoff@gmail.com>
2023-06-06 23:34:17 +0100

    feat: updated getlistingbyid hook

--- app/actions/getListingByIds.ts
+++ app/actions/getListingByIds.ts
@@ -4,17 +4,31 @@ import prisma from "../libs/prismadb"
 export default async function getListingId(params: IParams) {
   const {listingId} = params
 
-  const listing = await prisma.listings.findUnique({
-    where: {
-      id: listingId,
-    },
-    include: {
-      user: true,
-    },
-  })
+  try {
+    const listing = await prisma.listings.findUnique({
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true,
+      },
+    })
 
-  if (!listing) {
-    return null
+    if (!listing) {
+      return null
+    }
+
+    return {
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt.toISOString(),
+        updatedAt: listing.user.updatedAt.toISOString(),
+        emailVerified: listing.user.emailVerified?.toISOString() || null,
+      },
+    }
+  } catch (error: any) {
+    throw new Error(error)
   }
-  return listing
 }
