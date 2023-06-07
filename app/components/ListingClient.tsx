77cb309cbe9e266d992c308ee5271990cb78261b
Precioussoul <habsoff@gmail.com>
2023-06-07 23:01:20 +0100

    feat: added category variable that store useMemo returned value

--- app/components/ListingClient.tsx
+++ app/components/ListingClient.tsx
@@ -1,12 +1,18 @@
 "use client"
 
+import {useMemo} from "react"
 import {ListingClientProps} from "../types"
+import {categories} from "./Navbar/Categories"
 
 const ListingClient = ({
   listing,
   currentUser,
   reservation,
 }: ListingClientProps) => {
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category)
+  }, [listing.category])
+
   return <div>ListingClient</div>
 }
 
