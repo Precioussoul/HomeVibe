18fe1e12e5172c3015f006aa616d3e434ec3342e
Precioussoul <habsoff@gmail.com>
2023-06-07 23:06:45 +0100

    fix: Fixed ListingHead component not existing

---
+++ app/components/LIstingHead.tsx
@@ -0,0 +1,8 @@
+"use client"
+import React from "react"
+
+const ListingHead = () => {
+  return <div></div>
+}
+
+export default ListingHead
--- app/components/ListingClient.tsx
+++ app/components/ListingClient.tsx
@@ -3,6 +3,8 @@
 import {useMemo} from "react"
 import {ListingClientProps} from "../types"
 import {categories} from "./Navbar/Categories"
+import Container from "./Container"
+import ListingHead from "./LIstingHead"
 
 const ListingClient = ({
   listing,
@@ -13,7 +15,21 @@ const ListingClient = ({
     return categories.find((item) => item.label === listing.category)
   }, [listing.category])
 
-  return <div>ListingClient</div>
+  return (
+    <Container>
+      <div className='max-w-screen-lg mx-auto'>
+        <div className='flex flex-col gap-6'>
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </Container>
+  )
 }
 
 export default ListingClient
