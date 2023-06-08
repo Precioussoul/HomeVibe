f5d697e54ec561fdc2ad345c179eb5e4f50a9edc
Precioussoul <habsoff@gmail.com>
2023-06-08 18:56:00 +0100

    feat: completed listingHead and ListingClient functionality

--- app/components/LIstingHead.tsx
+++ app/components/LIstingHead.tsx
@@ -3,6 +3,8 @@ import React from "react"
 import {ListingHeadProps} from "../types"
 import Heading from "./Heading"
 import useCountries from "../hooks/useCountries"
+import Image from "next/image"
+import HeartButton from "./HeartButton"
 
 const ListingHead = ({
   locationValue,
@@ -19,6 +21,17 @@ const ListingHead = ({
         title={title}
         subtitle={`${location?.region}, ${location?.label}`}
       />
+      <div className='w-full h-[60vh] mt-3 overflow-hidden rounded-xl relative'>
+        <Image
+          src={imageSrc}
+          alt={"Image"}
+          fill
+          className='object-cover w-full'
+        />
+        <div className='absolute top-5 right-5'>
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
     </div>
   )
 }
--- app/components/ListingClient.tsx
+++ app/components/ListingClient.tsx
@@ -26,6 +26,13 @@ const ListingClient = ({
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+            />
+          </div>
         </div>
       </div>
     </Container>
