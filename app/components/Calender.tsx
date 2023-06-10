3a015957ce0fa10c2b309ae3b1a6709cc85a0fca
Precioussoul <habsoff@gmail.com>
2023-06-10 15:21:32 +0100

    featL : added typed for date-ramge package

---
+++ app/components/Calender.tsx
@@ -0,0 +1,8 @@
+"use client"
+import React from "react"
+
+const Calender = () => {
+  return <div>Calender</div>
+}
+
+export default Calender
--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import {Range} from "react-date-range"
 import {ListingReservationProps} from "@/app/types"
 
 const ListingReservation = ({
@@ -11,7 +11,20 @@ const ListingReservation = ({
   onSubmit,
   disabledDates,
 }: ListingReservationProps) => {
-  return <div>ListingReservation</div>
+  return (
+    <div className='bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden'>
+      <div className='flex flex-row items-center gap-1 p-4'>
+        <p className='text-2xl font-semibold'>${price}</p>
+        <p className='font-light text-neutral-500'>Night</p>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        disabledDate={disabledDates}
+        onChange={(value) => onChangeDate}
+      />
+    </div>
+  )
 }
 
 export default ListingReservation
