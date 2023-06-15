2b105d6477ac15b5d483002370992fb8d0b96350
Precioussoul <habsoff@gmail.com>
2023-06-15 22:58:05 +0100

    feat: added reservations client component and logic behind it

---
+++ app/reservations/page.tsx
@@ -0,0 +1,37 @@
+import ClientOnly from "../components/ClientOnly"
+import getCurrentUser from "../actions/getCurrentUser"
+import getReservations from "../actions/getReservations"
+import EmptyState from "../components/EmptyState"
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title='unauthorized' subtitle='Please login' />
+      </ClientOnly>
+    )
+  }
+
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  })
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title='No reservation found'
+          subtitle='looks like you have no reservations on your property'
+        />
+      </ClientOnly>
+    )
+  }
+
+  return (
+    <ClientOnly>
+      <ReservationsClient />
+    </ClientOnly>
+  )
+}
