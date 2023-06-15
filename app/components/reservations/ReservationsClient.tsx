35f00464977f9a33925765c409f71cb23769c1d4
Precioussoul <habsoff@gmail.com>
2023-06-15 23:09:44 +0100

    feat: importing reuseable component to make up reservationClient component

--- app/components/reservations/ReservationsClient.tsx
+++ app/components/reservations/ReservationsClient.tsx
@@ -1,5 +1,11 @@
+import {useRouter} from "next/navigation"
+import {useCallback, useState} from "react"
+import {SafeReservations, SafeUser} from "@/app/types"
+
 const ReservationsClient = () => {
-  return <div>ReservationsClient</div>
+  const router = useRouter()
+
+  return <div>Reservations on my properties </div>
 }
 
 export default ReservationsClient
