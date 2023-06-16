c593aa4b30302c4d660ad2499b2f5cef9571a338
Precioussoul <habsoff@gmail.com>
2023-06-16 22:07:23 +0100

    feat: added properties navigation to navbar

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -68,7 +68,10 @@ const UserMenu = ({currentUser}: UsermenuProps) => {
                   onClick={() => router.push("/reservations")}
                   label='My reservation'
                 />
-                <MenuItem onClick={() => {}} label='My properties' />
+                <MenuItem
+                  onClick={() => router.push("/properties")}
+                  label='My properties'
+                />
                 <MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
                 <MenuItem onClick={() => signOut()} label='Logout' />
               </>
