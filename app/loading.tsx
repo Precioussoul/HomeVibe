56364d8f8385dea810b06664dc496e184c4bd1cc
Precioussoul <habsoff@gmail.com>
2023-06-17 21:23:06 +0100

    feat: implemented middleware to protect our routes

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const Loading = () => {
   return (
-    <div className=''>
+    <div className='h-[70vh] w-full items-center flex flex-col justify-center'>
       <div className='loader animate-spin'></div>
     </div>
   )
