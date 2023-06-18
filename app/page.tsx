fb177d5c3c837dbc0ac2d7ef0c2752388fb2313e
Precioussoul <habsoff@gmail.com>
2023-06-18 21:46:30 +0100

    fix: fixed responsiveness issue on grid

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home({searchParams}: HomeProps) {
   return (
     <>
       <Container>
-        <div className='pt-[4.5rem] gap-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+        <div className='pt-[4.5rem] gap-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 laptopL:grid-cols-5 2xl:grid-cols-6'>
           {listings.map((listing) => (
             <div key={listing.id}>
               <ListingCard data={listing} currentUser={currentUser} />
